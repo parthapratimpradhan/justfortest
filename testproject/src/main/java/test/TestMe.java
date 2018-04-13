97eb88c1801e1dd2c97d59a65b5d2dc0ecd63bb8
Partha Pratim Pradhan <partha.pradhan@mheducation.com>
2018-04-13 09:33:58 +0530

    implemented add function

--- testproject/src/main/java/test/TestMe.java
+++ testproject/src/main/java/test/TestMe.java
@@ -3,8 +3,16 @@ package test;
 public class TestMe {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		System.out.println(add(10,20));
 
+		
+	}
+	
+	
+	private static int add(int a, int b){
+		return a+b;
+		
 	}
 
 }
