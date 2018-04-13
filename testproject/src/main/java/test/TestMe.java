7e800d9aa06bb9b14fc2f16c487988502c5722af
Partha Pratim Pradhan <partha.pradhan@mheducation.com>
2018-04-13 10:08:25 +0530

    homepage testing added

--- testproject/src/main/java/test/TestMe.java
+++ testproject/src/main/java/test/TestMe.java
@@ -5,14 +5,24 @@ public class TestMe {
 	public static void main(String[] args) {
 	
 		System.out.println(add(10,20));
+		
+		homePageTesting();
 
 		
 	}
 	
 	//Add comment to function
 	private static int add(int a, int b){
+		
+		
 		return a+b;
 		
+		
+	}
+	
+	private static void homePageTesting(){
+		
+		System.out.println("Home page testing done succefully");
 	}
 
 }
